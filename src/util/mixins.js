7428a1052875bffcba0a8c8419ba986c4bac7df8
rancyTao <emmily_tao@163.com>
2019-08-18 12:03:26 +0800

    add

--- src/util/mixins.js
+++ src/util/mixins.js
@@ -9,39 +9,35 @@ export const permission = {
      * 调用API获取当前用户信息，并根据组装左侧菜单数据，存入vuex
      */
     getInfo() {
-      this.$ajax.get(
-        this,
-        this.$api.GET_USER_INFO, {},
-        null,
-        (res) => {
-          console.log(res)
-          // 本地存储用户基本信息
-          let userInfo = res.data.content,
-            name = userInfo.username = res.data.content.name||res.data.content.phone
-          this.$cookie.set('userInfo', userInfo)
-          this.$cookie.set('userName', name)
-          this.$store.commit('SET_USERNAME', name)
+      this.$ajax.get({
+        url: this.$api.GET_USER_INFO,
+        params: {}
+      }).then(res => {
+        console.log(res)
+        // 本地存储用户基本信息
+        let userInfo = res.data.content,
+          name = userInfo.username = res.data.content.name||res.data.content.phone
+        this.$cookie.set('userInfo', userInfo)
+        this.$cookie.set('userName', name)
+        this.$store.commit('SET_USERNAME', name)
 
-          // 此处应API获取用户权限信息，先暂写死权限信息
-          this.$ajax.get(
-            this,
-            this.$api.GET_USER_PEIMISSION, {},
-            null,
-            (res) => {
-              // todo: 目前写死权限，是否有全部权限
-              const isAllPerm = false
-              // 当前用户全部权限编码，包含菜单及功能操作
-              let authCodeList = [ 'S0501', 'S050101', 'S050102', 'S050102', 'S050103', 'S050104', 'S050105', 'S050106', 'S050107', 'SCYJ','YQCB','NYPC','ZXJC' ]
-              let authMenuAll = []
-              // 写入vuex
-              this.$store.commit('SET_MENU', {authMenuAll , authCodeList, isAllPerm})
-              // 根据获取的权限信息，从router中获取左侧菜单信息，并重新赋值vuex
-              this.getPermission(isAllPerm)
-            },
-          )
-        },
-      )
+        // 此处应API获取用户权限信息，先暂写死权限信息
+        this.$ajax.get({
+          url: this.$api.GET_USER_PEIMISSION,
+          params: {}
+        }).then(res=>{
+          // todo: 目前写死权限，是否有全部权限
+          const isAllPerm = false
+          // 当前用户全部权限编码，包含菜单及功能操作
+          let authCodeList = [ 'S0501', 'S050101', 'S050102', 'S050102', 'S050103', 'S050104', 'S050105', 'S050106', 'S050107', 'SCYJ','YQCB','NYPC','ZXJC' ]
+          let authMenuAll = []
+          // 写入vuex
+          this.$store.commit('SET_MENU', {authMenuAll , authCodeList, isAllPerm})
+          // 根据获取的权限信息，从router中获取左侧菜单信息，并重新赋值vuex
+          this.getPermission(isAllPerm)
+        })
 
+      })
     },
     /**
      * 组装左侧菜单
@@ -69,11 +65,15 @@ export const permission = {
           this.$store.commit('SET_MENU', {authMenuAll, authCodeList, isAllPerm})
         }else{ // vuex中不存在权限码信息，需重新调用接口再获取
 
-          // todo:调用权限获取接口，重新获取权限编码
-          let authCodeList = [ 'S0501', 'S050101', 'S050102', 'S050102', 'S050103', 'S050104', 'S050105', 'S050106', 'S050107', 'SCYJ','YQCB','NYPC','ZXJC' ]
-          authMenuAll = getSideMenu(routes,authCodeList)
-          this.$store.commit('SET_MENU', {authMenuAll, authCodeList, isAllPerm})
-
+          this.$ajax.get({
+            url: this.$api.GET_USER_PEIMISSION,
+            params: {}
+          }).then(res=>{
+            // todo:调用权限获取接口，重新获取权限编码
+            let authCodeList = [ 'S0501', 'S050101', 'S050102', 'S050102', 'S050103', 'S050104', 'S050105', 'S050106', 'S050107', 'SCYJ','YQCB','NYPC','ZXJC' ]
+            authMenuAll = getSideMenu(routes,authCodeList)
+            this.$store.commit('SET_MENU', {authMenuAll, authCodeList, isAllPerm})
+          })
         }
       }
     },
