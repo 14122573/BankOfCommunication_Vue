30ab02558207b17c06ed3f8684d3cda2afed5b37
Zjja <2736839008@qq.com>
2019-08-23 18:07:23 +0800

    新增页面

--- src/views/systemManagement/administrator/new-user-create.vue
+++ src/views/systemManagement/administrator/new-user-create.vue
@@ -1,9 +1,199 @@
 <template>
-	<div></div>
+	<a-card :bordered='false'>
+		<a-row type="flex" justify="space-between" slot='title'>
+			<a-col>
+				新增用户信息
+			</a-col>
+			<a-col>
+				<a-button type="primary" ghost @click='handleReturn'>返回</a-button>
+				<a-button type="primary" @click='handleAdd'>保存</a-button>
+			</a-col>
+		</a-row>
+		<a-form :form="searchForm">
+			<a-row type="flex" justify="space-between">
+				<a-col span="8">
+					<a-form-item label="姓名：" :label-col="labelCol" :wrapper-col="wrapperCol">
+						<a-input placeholder="请输入" v-model="searchForm.name" />
+					</a-form-item>
+				</a-col>
+				<a-col span="8">
+					<a-form-item label="账号：" :label-col="labelCol" :wrapper-col="wrapperCol">
+						<a-input placeholder="请输入" v-model="searchForm.name" />
+					</a-form-item>
+				</a-col>
+				<a-col span="8">
+					<a-form-item label="邮箱" :label-col="labelCol" :wrapper-col="wrapperCol">
+						<a-input placeholder="请输入" v-model="searchForm.name" />
+					</a-form-item>
+				</a-col>
+			</a-row>
+			<a-row type="flex" justify="start">
+				<a-col span="8">
+					<a-form-item label="邮编：" :label-col="labelCol" :wrapper-col="wrapperCol">
+						<a-input placeholder="请输入" v-model="searchForm.name" />
+					</a-form-item>
+				</a-col>
+			</a-row>
+			<a-row type="flex" justify="start">
+				<a-col span="16">
+					<a-form-item label="单位名称：" :label-col="{span:3}" :wrapper-col="wrapperCol">
+						<a-input placeholder="请输入" v-model="searchForm.name" />
+					</a-form-item>
+				</a-col>
+			</a-row>
+			<a-row type="flex" justify="start">
+				<a-col span="16">
+					<a-form-item label="地址：" :label-col="{span:3}" :wrapper-col="wrapperCol">
+						<a-input placeholder="请输入" v-model="searchForm.name" />
+					</a-form-item>
+				</a-col>
+			</a-row>
+			<a-divider dashed />
+			<a-row type="flex" justify="space-between">
+				<a-col span="8">
+					<a-form-item label="角色名称：" :label-col="labelCol" :wrapper-col="wrapperCol">
+						<a-input placeholder="请输入" v-model="searchForm.name" />
+					</a-form-item>
+				</a-col>
+				<a-col span="8">
+					<a-form-item label="组织机构：" :label-col="labelCol" :wrapper-col="wrapperCol">
+						<a-input placeholder="请输入" v-model="searchForm.name" />
+					</a-form-item>
+				</a-col>
+				<a-col span="8">
+					<a-form-item label="所属区域：" :label-col="labelCol" :wrapper-col="wrapperCol">
+						<a-input placeholder="请输入" v-model="searchForm.name" />
+					</a-form-item>
+				</a-col>
+			</a-row>
+		</a-form>
+		<a-tree class="tree" checkable @expand="onExpand" :expandedKeys="expandedKeys" :autoExpandParent="autoExpandParent"
+		 v-model="checkedKeys" @select="onSelect" :selectedKeys="selectedKeys" :treeData="treeData" />
+	</a-card>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      searchForm: {
+        name: ''
+      },
+      dateFormat: 'YYYY-MM-DD',
+      labelCol: {
+        span: 6
+      },
+      wrapperCol: {
+        span: 15
+      },
+      autoExpandParent: true,
+      checkedKeys: [],
+      expandedKeys: [],
+      selectedKeys: [],
+      treeData: [{
+        title: '0-0',
+        key: '0-0',
+        children: [{
+          title: '0-0-0',
+          key: '0-0-0',
+          children: [{
+            title: '0-0-0-0',
+            key: '0-0-0-0'
+          },
+          {
+            title: '0-0-0-1',
+            key: '0-0-0-1'
+          },
+          {
+            title: '0-0-0-2',
+            key: '0-0-0-2'
+          },
+          ],
+        }, {
+          title: '0-0-1',
+          key: '0-0-1',
+          children: [{
+            title: '0-0-1-0',
+            key: '0-0-1-0'
+          },
+          {
+            title: '0-0-1-1',
+            key: '0-0-1-1'
+          },
+          {
+            title: '0-0-1-2',
+            key: '0-0-1-2'
+          },
+          ],
+        }, {
+          title: '0-0-2',
+          key: '0-0-2',
+        }],
+      }, {
+        title: '0-1',
+        key: '0-1',
+        children: [{
+          title: '0-1-0-0',
+          key: '0-1-0-0'
+        },
+        {
+          title: '0-1-0-1',
+          key: '0-1-0-1'
+        },
+        {
+          title: '0-1-0-2',
+          key: '0-1-0-2'
+        },
+        ],
+      }, {
+        title: '0-2',
+        key: '0-2',
+      }]
+    }
+  },
+  watch: {
+    checkedKeys(val) {
+      console.log('onCheck', val)
+    }
+  },
+  methods: {
+    onExpand(expandedKeys) {
+      console.log('onExpand', expandedKeys)
+      // this.expandedKeys.push(expandedKeys);
+      // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+      // or, you can remove all expanded children keys.
+      this.expandedKeys = expandedKeys
+      // this.autoExpandParent = false
+    },
+    onCheck(checkedKeys) {
+      console.log('onCheck', checkedKeys)
+      this.checkedKeys = checkedKeys
+    },
+    onSelect(selectedKeys, info) {
+      console.log('onSelect', info)
+      this.selectedKeys = selectedKeys
+    },
+    handleReturn(){
+      this.$router.push({
+        name:'/systemManagement/administrator'
+      })
+    },
+    handleSave(){
+				
+    }
+  }
+}
 </script>
 
+<style scoped>
+
+</style>
 <style>
+	.tree>li {
+		background: rgba(247, 247, 247);
+	}
+
+	.tree li ul {
+		background: white;
+	}
 </style>
--- src/views/systemManagement/administrator/new-user-view.vue
+++ src/views/systemManagement/administrator/new-user-view.vue
@@ -1,9 +1,215 @@
 <template>
-	<div></div>
+	<a-card :bordered='false'>
+		<a-row type="flex" justify="space-between" slot='title'>
+			<a-col>
+				查看新用户信息
+			</a-col>
+			<a-col>
+				<a-button type="primary" ghost @click='handleReturn'>返回</a-button>
+				<a-button type="primary" ghost @click='handleAdd'>修改</a-button>
+			</a-col>
+		</a-row>
+		<a-row type="flex" justify="start">
+			<a-col span="8">
+				<a-row type="flex" justify="start" class="colMargin">
+					<a-col span="7" class="colLabel">
+						姓名：
+					</a-col>
+					<a-col span="17">
+						姓名
+					</a-col>
+				</a-row>
+			</a-col>
+			<a-col span="8">
+				<a-row type="flex" justify="start" class="colMargin">
+					<a-col span="7" class="colLabel">
+						账户：
+					</a-col>
+					<a-col span="17">
+						账户
+					</a-col>
+				</a-row>
+			</a-col>
+			<a-col span="8">
+				<a-row type="flex" justify="start" class="colMargin">
+					<a-col span="7" class="colLabel">
+						邮箱：
+					</a-col>
+					<a-col span="17">
+						邮箱
+					</a-col>
+				</a-row>
+			</a-col>
+		</a-row>
+		<a-row>
+			<a-col span="8">
+				<a-row type="flex" justify="start" class="colMargin">
+					<a-col span="7" class="colLabel">
+						邮编：
+					</a-col>
+					<a-col span="17">
+						邮编
+					</a-col>
+				</a-row>
+			</a-col>
+		</a-row>
+		<a-row>
+			<a-col span="8">
+				<a-row type="flex" justify="start" class="colMargin">
+					<a-col span="7" class="colLabel">
+						单位名称：
+					</a-col>
+					<a-col span="17">
+						单位名称
+					</a-col>
+				</a-row>
+			</a-col>
+		</a-row>
+		<a-row>
+			<a-col span="8">
+				<a-row type="flex" justify="start" class="colMargin">
+					<a-col span="7" class="colLabel">
+						地址：
+					</a-col>
+					<a-col span="17">
+						地址
+					</a-col>
+				</a-row>
+			</a-col>
+		</a-row>
+		<a-divider dashed />
+		<a-row>
+			<a-col span="8">
+				<a-row type="flex" justify="start" class="colMargin">
+					<a-col span="7" class="colLabel">
+						角色名称：
+					</a-col>
+					<a-col span="17">
+						角色名称
+					</a-col>
+				</a-row>
+			</a-col>
+			<a-col span="8">
+				<a-row type="flex" justify="start" class="colMargin">
+					<a-col span="7" class="colLabel">
+						组织机构名称：
+					</a-col>
+					<a-col span="17">
+						组织机构名称
+					</a-col>
+				</a-row>
+			</a-col>
+			<a-col span="8">
+				<a-row type="flex" justify="start" class="colMargin">
+					<a-col span="7" class="colLabel">
+						所属行政区域：
+					</a-col>
+					<a-col span="17">
+						所属行政区域
+					</a-col>
+				</a-row>
+			</a-col>
+		</a-row>
+		<a-tree class="tree" checkable :defaultExpandAll="defaultExpandAll" v-model="checkedKeys"
+		 :selectedKeys="selectedKeys" :treeData="treeData" disabled/>
+	</a-card>
 </template>
 
 <script>
-</script>
+export default {
+  data() {
+    return {
+      checkedKeys: [],
+      selectedKeys: [],
+      defaultExpandAll:true,
+      treeData: [{
+        title: '0-0',
+        key: '0-0',
+        children: [{
+          title: '0-0-0',
+          key: '0-0-0',
+          children: [{
+            title: '0-0-0-0',
+            key: '0-0-0-0'
+          },
+          {
+            title: '0-0-0-1',
+            key: '0-0-0-1'
+          },
+          {
+            title: '0-0-0-2',
+            key: '0-0-0-2'
+          },
+          ],
+        }, {
+          title: '0-0-1',
+          key: '0-0-1',
+          children: [{
+            title: '0-0-1-0',
+            key: '0-0-1-0'
+          },
+          {
+            title: '0-0-1-1',
+            key: '0-0-1-1'
+          },
+          {
+            title: '0-0-1-2',
+            key: '0-0-1-2'
+          },
+          ],
+        }, {
+          title: '0-0-2',
+          key: '0-0-2',
+        }],
+      }, {
+        title: '0-1',
+        key: '0-1',
+        children: [{
+          title: '0-1-0-0',
+          key: '0-1-0-0'
+        },
+        {
+          title: '0-1-0-1',
+          key: '0-1-0-1'
+        },
+        {
+          title: '0-1-0-2',
+          key: '0-1-0-2'
+        },
+        ],
+      }, {
+        title: '0-2',
+        key: '0-2',
+      }]
+    }
+  },
+  methods: {
+    handleReturn() {
+      this.$router.push({
+        name: '/systemManagement/administrator'
+      })
+    },
+    handleAdd() {
 
+    }
+  }
+}
+</script>
+<style scoped>
+	.colMargin {
+		margin-bottom: 20px;
+	}
+
+	.colLabel {
+		text-align: right;
+	}
+</style>
 <style>
+	.tree>li {
+		background: rgba(247, 247, 247);
+	}
+
+	.tree li ul {
+		background: white;
+	}
 </style>
