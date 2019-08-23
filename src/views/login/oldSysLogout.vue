8c6899ea4253201c960ae60dce303e517e2a59fb
rancyTao <emmily_tao@163.com>
2019-08-23 14:50:44 +0800

    fix bug

--- src/views/login/oldSysLogout.vue
+++ src/views/login/oldSysLogout.vue
@@ -29,33 +29,13 @@ export default {
         this.$com.handleLogOut()
       }else{
         this.$ajax.get({
-          url: this.$api.GET_USER_PEIMISSION
-        }).then(res=>{
+          url: this.$api.GET_USER_INFO
+        }).then(res => {
           if(!res.data || !res.data.content){
             this.$com.handleLogOut()
           }else{
-            let authCodes = res.data.content
-            let hasNewSysAuth = false
-            if(Array.isArray(authCodes)){
-              if(authCodes.length ==0)
-                for(let i=0;i<authCodes.length;i++){
-                  if(authCodes[i].indexOf('S')>0){
-                    hasNewSysAuth = true
-                  }
-                }
-            }
-
-            if(hasNewSysAuth){
-              this.$router.push({name:'home'})
-            }else{
-              if(this.verOldSysAuth(checkResult)){
-                this.$cookie.remove('token')
-                this.$cookie.remove('refresh_token')
-                this.$router.push({name:'bindPhone',query:{ logined:1 }})
-              }else{
-                this.$com.handleLogOut()
-              }
-            }
+            let sysList = this.getLastOldSysAuth(checkResult,res.data.content.oldAccountSet)
+            this.checkPortalAuth(sysList)
           }
         })
       }
@@ -63,8 +43,71 @@ export default {
   },
   methods: {
     /**
+     * @param {Object} orgOldSysList 从cookie里取出来的，登录绑定前获取到的老系统清单
+     * @param {Object} newOldSysList 登录系统后，通过userInfo获取的最新老系统清单
+     */
+    getLastOldSysAuth(orgOldSysList,newOldSysList){
+      orgOldSysList = !orgOldSysList?[]:orgOldSysList
+      newOldSysList = Array.isArray(newOldSysList)?newOldSysList:[]
+
+      if(Array.isArray(orgOldSysList)){
+        let resultSysList = []
+        for(let i=0;i<orgOldSysList.length;i++){
+          let oldSysItem = orgOldSysList[i]
+          let isFound = false
+          for(let j=0;j<newOldSysList.length;j++){
+            let newSysItem = newOldSysList[j]
+            if(oldSysItem.sysDic.sysCode == newSysItem.sysDic.sysCode ){
+              resultSysList.push(newSysItem)
+              isFound = true
+            }
+          }
+          if(!isFound) {
+            resultSysList.push(oldSysItem)
+          }
+        }
+        return resultSysList
+      }else {
+        return []
+      }
+    },
+    checkPortalAuth(oldSysList){
+      oldSysList = !oldSysList?[]:oldSysList
+
+      this.$ajax.get({
+        url: this.$api.GET_USER_PEIMISSION
+      }).then(res=>{
+        if(!res.data || !res.data.content){
+          this.$com.handleLogOut()
+        }else{
+          let authCodes = res.data.content
+          let hasNewSysAuth = false
+          if(Array.isArray(authCodes)){
+            if(authCodes.length ==0)
+              for(let i=0;i<authCodes.length;i++){
+                if(authCodes[i].indexOf('S')>0){
+                  hasNewSysAuth = true
+                }
+              }
+          }
+
+          if(hasNewSysAuth){
+            this.$router.push({name:'home'})
+          }else{
+            if(this.verOldSysAuth(oldSysList)){
+              this.$cookie.remove('token')
+              this.$cookie.remove('refresh_token')
+              this.$router.push({name:'bindPhone',query:{ logined:1 }})
+            }else{
+              this.$com.handleLogOut()
+            }
+          }
+        }
+      })
+    },
+    /**
      * 验证当前从老系统【安全退出】的用户，是否拥有老系统权限。并在有多个老系统权限的情况下，跳转重选系统界面
-     * @param {Object} 当前用户下拥有的老系统清单
+     * @param {Object} oldSys 当前用户下拥有的老系统清单
      * @return {Boolean} true：有多个老系统权限； false：没有或者有一个老系统权限
      */
     verOldSysAuth(oldSys){
