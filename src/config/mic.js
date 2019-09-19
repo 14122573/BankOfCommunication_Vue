6b1b161dd50ca60af54a448f93354178cfa4098b
rancyTao <emmily_tao@163.com>
2019-09-19 18:18:29 +0800

    add

--- src/config/mic.js
+++ src/config/mic.js
@@ -63,7 +63,7 @@ case 'production': // 生产环境下
 default: // 默认环境下（开发环境）
   MicConfig = [
     {
-      baseUrl:'http://scsd.omniview.pro',
+      baseUrl:'http://scsd.tao.com',
       resourceName:'scsd',
       micId:'mic:scsd',
       pathPrefix:'/scsd'
