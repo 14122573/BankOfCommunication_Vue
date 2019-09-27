ff211594fd00fa89e4c120bd066dfbce5de22092
陶仁倩 <emmily_tao@163.com>
2019-09-27 08:21:43 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,6 @@ export default {
   }
 }
 </script>
-
 <style>
   #portal {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;color: #2c3e50;width: 100%;height: 100%;
