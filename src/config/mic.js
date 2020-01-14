0d8b4a841f92ca2b3eb35373c897035095e74021
rancyTao <emmily_tao@163.com>
2020-01-14 11:11:46 +0800

    完全修改子系统接入配置读取方式

--- src/config/mic.js
+++ src/config/mic.js
@@ -1,191 +1,26 @@
 /**
- * 配置需注册子项目的mic名称
- * 参数说明：
- *  baseUrl：子项目部署服务后，公网路由域名
- *  resourceName：子项目注册进Portal后的独立资源空间名称
- *  micId：子项目打包部署时自行定义的名称
- *  pathPrefix：子项目路由base参数
+ * 判断store中是否存在子项目资源，不存在则远程调用接口存入store
  */
 
-let MicConfig = []
-switch (process.env.NODE_ENV) {
-case 'devol': // 本地线上部署环境下
-  MicConfig = [
-    {
-      baseUrl: 'http://scsd.tao.com',
-      resourceName: 'scsd',
-      micId: 'mic:scsd',
-      pathPrefix: '/scsd'
-    },
-    {
-      baseUrl: 'http://txc.tao.com', // 科普基地
-      resourceName: 'kpjd',
-      micId: 'mic:kpjd',
-      pathPrefix: '/kpjd'
-    },
-    {
-      baseUrl: 'http://scylzc.tao.com', // 原良种验收
-      resourceName: 'scylzc',
-      micId: 'mic:scylzc',
-      pathPrefix: '/scylzc'
-    },
-    {
-      baseUrl: 'http://ylzcre.tao.com', // 原良种复查
-      resourceName: 'ylzcre',
-      micId: 'mic:ylzcre',
-      pathPrefix: '/ylzcre'
-    },
-    {
-      baseUrl: 'http://yysygl.tao.com',// 渔业兽医
-      resourceName: 'yysygl',
-      micId: 'mic:yysygl',
-      pathPrefix: '/yysygl'
-    },
-    {
-      baseUrl: 'http://fljps.tao.com',// 范蠡奖
-      resourceName: 'fljps',
-      micId: 'mic:fljps',
-      pathPrefix: '/fljps'
-    }
-  ]
-  break
-case 'sit': // sit环境下
-  MicConfig = [{
-    baseUrl: 'http://scsd.omniview.pro', //新品种
-    resourceName: 'scsd',
-    micId: 'mic:scsd',
-    pathPrefix: '/scsd'
-  },
-  {
-    baseUrl: 'http://scylzc.omniview.pro', //原良种验收
-    resourceName: 'scylzc',
-    micId: 'mic:scylzc',
-    pathPrefix: '/scylzc'
-  },
-  {
-    baseUrl: 'http://ylzcre.omniview.pro', //原良种复查
-    resourceName: 'ylzcre',
-    micId: 'mic:ylzcre',
-    pathPrefix: '/ylzcre'
-  },
-  {
-    baseUrl: 'http://bbgl.omniview.pro', //智慧报表
-    resourceName: 'bbgl',
-    micId: 'mic:bbgl',
-    pathPrefix: '/bbgl'
-  },
-  {
-    baseUrl: 'http://zlzs.omniview.pro', //质量安全追述
-    resourceName: 'zlzs',
-    micId: 'mic:zlzs',
-    pathPrefix: '/zlzs'
-  },
-  {
-    baseUrl: 'http://yysygl.omniview.pro', //智慧报表
-    resourceName: 'yysygl',
-    micId: 'mic:yysygl',
-    pathPrefix: '/yysygl'
-  },
-  {
-    baseUrl: 'http://kpjd.omniview.pro', // 科普基地
-    resourceName: 'kpjd',
-    micId: 'mic:kpjd',
-    pathPrefix: '/kpjd'
-  },
-  {
-    baseUrl: 'http://xshy.omniview.pro', // 学术会议
-    resourceName: 'xshy',
-    micId: 'mic:xshy',
-    pathPrefix: '/xshy'
-  },
-  {
-    baseUrl: 'http://iftp.omniview.pro/lshy', // 理事会议
-    resourceName: 'lshy',
-    micId: 'mic:lshy',
-    pathPrefix: '/lshy'
-  },
-  {
-    baseUrl: 'http://ssdw.omniview.pro', // 水生动物防疫系统实验室能力验证
-    resourceName: 'ssdw',
-    micId: 'mic:ssdw',
-    pathPrefix: '/ssdw'
-  },
-  {
-    baseUrl: 'http://pjsf.omniview.pro', // 中国水产学会团体标准管理
-    resourceName: 'ttbz',
-    micId: 'mic:ttbz',
-    pathPrefix: '/ttbz'
-  },
-  {
-    baseUrl: 'http://fljps.omniview.pro', // 范蠡奖评审
-    resourceName: 'fljps',
-    micId: 'mic:fljps',
-    pathPrefix: '/fljps'
-  },
-  {
-    baseUrl: 'http://iftp.omniview.pro/dyzh', // 稻鱼综合种养
-    resourceName: 'dyzh',
-    micId: 'mic:dyzh',
-    pathPrefix: '/dyzh'
-  },
-  {
-    baseUrl: 'http://iftp.omniview.pro/hymc', // 海洋牧场
-    resourceName: 'hymc',
-    micId: 'mic:hymc',
-    pathPrefix: '/hymc'
-  },
-  {
-    baseUrl: 'http://iftp.omniview.pro/xxyy', // 休闲渔业
-    resourceName: 'xxyy',
-    micId: 'mic:xxyy',
-    pathPrefix: '/xxyy'
-  },
-  {
-    baseUrl: 'http://iftp.omniview.pro/styy', // 大水面生态渔业
-    resourceName: 'styy',
-    micId: 'mic:styy',
-    pathPrefix: '/styy'
-  },
-  ]
-  break
-case 'uat': // uat环境下
-  MicConfig = [{
-    baseUrl: 'http://scsd.szjoin.net',
-    resourceName: 'scsd',
-    micId: 'mic:scsd',
-    pathPrefix: '/scsd'
-  }]
-  break
-case 'production': // 生产环境下
-  MicConfig = [{ //质量安全/追述
-    baseUrl: 'http://znyj.nftec.agri.cn/zlaq',
-    resourceName: 'zlzs',
-    micId: 'mic:zlzs',
-    pathPrefix: '/zlzs'
-  }]
-  break
-default: // 默认环境下（开发环境）
-  MicConfig = [{
-    baseUrl: 'http://scsd.tao.com',
-    resourceName: 'scsd',
-    micId: 'mic:scsd',
-    pathPrefix: '/scsd'
-  },{
-    baseUrl: 'http://scylzc.tao.com', // 原良种验收
-    resourceName: 'scylzc',
-    micId: 'mic:scylzc',
-    pathPrefix: '/scylzc'
-  },
-  {
-    baseUrl: 'http://ylzcre.tao.com', // 原良种复查
-    resourceName: 'ylzcre',
-    micId: 'mic:ylzcre',
-    pathPrefix: '/ylzcre'
-  },
-  ]
-  break
-};
+import axios from 'axios'
+import API from '@/server/api'
+import Common from '@/util/common'
+import store from '@/store'
 
-export const MicConfigs = [
-  ...MicConfig,
-]
+if(!store.state.micSystemResourceConfig){
+  const newInstance = axios.create({
+    baseURL: '',
+    timeout: 5000,
+    headers: {'Content-type': 'multipart/form-data'}
+  })
+
+  newInstance.get(API.CONFIGS_MICSYSTEMS_LIST).then(res => {
+    if(res.status == 200){
+      // 获取成功，存储配置文件结果
+      let micSystemRecourceConfig = Common.confirm(res, 'data', {})
+      // let micSystemRecourceConfig = AllMicSystemsList[process.env.NODE_ENV=='development'?'sit':process.env.NODE_ENV]
+      store.commit('SET_MICSYSTEMS_RESOURCE_CONFIG', micSystemRecourceConfig)
+    }
+  })
+}
+
--- src/main-mic.js
+++ src/main-mic.js
@@ -3,9 +3,6 @@ import {registerApplication, start} from 'single-spa'
 import axios from 'axios'
 import API from '@/server/api'
 import Common from '@/util/common'
-// import fetchInject from 'fetch-inject'
-// import { MicConfigs } from '@/config/mic'
-// import com from '@/util/common'
 import store from './store'
 
 (async function loadApp(){
@@ -13,34 +10,55 @@ import store from './store'
   window.isSpa = true
   await registerApplication('layout', () => import('@/index.js'), () => true)
   window.onload = async () => {
-    // let progress = 0
-    // store.commit('SET_LOADING_PROGRESS', {progress, len: MicConfigs.length})
-    const newInstance = axios.create({
-      baseURL: '',
-      timeout: 5000,
-      headers: {'Content-type': 'multipart/form-data'}
-    })
-    newInstance.get(API.CONFIGS_MICSYSTEMS_LIST).then(res => {
-      if(res.status == 200){
-        // 获取成功，存储配置文件结果
-        let AllMicSystemsList = Common.confirm(res, 'data', {})
-        let MicSystemsList = AllMicSystemsList[process.env.NODE_ENV=='development'?'sit':process.env.NODE_ENV]
+    // 从store中获取 子系统资源加载配置信息。并申明变量
+    let micSystemRecourceConfig = store.state.micSystemResourceConfig
+    let MicSystemsList
 
-        // 当页面加载好了，有了#content元素后才加载子项目，避免刷新后空白的问题
-        let progress = 0
-        store.commit('SET_LOADING_PROGRESS', {progress, len: MicSystemsList.length})
-        // 循环已接入子系统配置
-        for(let i=0;i<MicSystemsList.length;i++){
-          // 读取子项目配置
-          Promise.resolve(loadResource(MicSystemsList[i].baseUrl)).then(() => {
-            registerApplication(MicSystemsList[i].resourceName, () => Promise.resolve(window[MicSystemsList[i].micId]),  pathPrefix(MicSystemsList[i].pathPrefix))
-            store.commit('SET_LOADING_PROGRESS', {progress: progress += 1, len: MicSystemsList.length})
-          }).catch(() => {
-            store.commit('SET_LOADING_PROGRESS', {progress: progress += 1, len: MicSystemsList.length})
-          })
+    if(!micSystemRecourceConfig){ // 当store中未存储过系统资源加载配置信息，远程获取存入store，同时加载子项目资源
+      const newInstance = axios.create({
+        baseURL: '',
+        timeout: 5000,
+        headers: {'Content-type': 'multipart/form-data'}
+      })
+      newInstance.get(API.CONFIGS_MICSYSTEMS_LIST).then(res => {
+        if(res.status == 200){
+          // 获取成功，存储配置文件结果
+          micSystemRecourceConfig = Common.confirm(res, 'data', {})
+          store.commit('SET_MICSYSTEMS_RESOURCE_CONFIG', micSystemRecourceConfig)
+          MicSystemsList = micSystemRecourceConfig[process.env.NODE_ENV=='development'?'sit':process.env.NODE_ENV]
+
+          // 当页面加载好了，有了#content元素后才加载子项目，避免刷新后空白的问题
+          let progress = 0
+          store.commit('SET_LOADING_PROGRESS', {progress, len: MicSystemsList.length})
+          // 循环已接入子系统配置
+          for(let i=0;i<MicSystemsList.length;i++){
+            // 读取子项目配置
+            Promise.resolve(loadResource(MicSystemsList[i].baseUrl)).then(() => {
+              registerApplication(MicSystemsList[i].resourceName, () => Promise.resolve(window[MicSystemsList[i].micId]),  pathPrefix(MicSystemsList[i].pathPrefix))
+              store.commit('SET_LOADING_PROGRESS', {progress: progress += 1, len: MicSystemsList.length})
+            }).catch(() => {
+              store.commit('SET_LOADING_PROGRESS', {progress: progress += 1, len: MicSystemsList.length})
+            })
+          }
         }
+      })
+    }else{// 当store中已存储过系统资源加载配置信息，直接加载子项目资源
+      MicSystemsList = micSystemRecourceConfig[process.env.NODE_ENV=='development'?'sit':process.env.NODE_ENV]
+      // 当页面加载好了，有了#content元素后才加载子项目，避免刷新后空白的问题
+      let progress = 0
+      store.commit('SET_LOADING_PROGRESS', {progress, len: MicSystemsList.length})
+      // 循环已接入子系统配置
+      for(let i=0;i<MicSystemsList.length;i++){
+        // 读取子项目配置
+        Promise.resolve(loadResource(MicSystemsList[i].baseUrl)).then(() => {
+          registerApplication(MicSystemsList[i].resourceName, () => Promise.resolve(window[MicSystemsList[i].micId]),  pathPrefix(MicSystemsList[i].pathPrefix))
+          store.commit('SET_LOADING_PROGRESS', {progress: progress += 1, len: MicSystemsList.length})
+        }).catch(() => {
+          store.commit('SET_LOADING_PROGRESS', {progress: progress += 1, len: MicSystemsList.length})
+        })
       }
-    })
+    }
+
 
   }
   start()
