55929c352f8524a54d3d098618446baf787889f4
Gakki <163alliance@gmail.com>
2020-07-30 16:26:09 +0800

    console删除

--- src/views/frontPublic/views/homePage/components/lunbo.vue
+++ src/views/frontPublic/views/homePage/components/lunbo.vue
@@ -157,7 +157,6 @@ export default {
      * @description 监听并用于切换LoginPanel, welcomepanel的状态
      */
     pageTypeChange(data) {
-      console.log(data)
       if(typeof(data) == 'object') {
         this.pageType = data.username
         this.username = data.username
@@ -182,13 +181,11 @@ export default {
           .then(res => {
             let userInfo = res.data.content
             if (!!userInfo.name) {
-              console.log('zaiqueren')
               this.username = userInfo.name
               this.pageType = userInfo.name
             }
           })
       } else {
-        console.log(cookie)
         // // 没有token有两种情况，一种是用户没登录，一种是老用户已登录
         // if(isOldSys = true) {
         //   this.username = "ok"
