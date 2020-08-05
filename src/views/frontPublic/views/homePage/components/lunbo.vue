e2847360bc629690b673f822e0aea85befc4ba28
阿璐 <763205056@qq.com>
2020-08-05 14:22:00 +0800

    fix(lunbo.vue):注释轮播图下方“login”标识

--- src/views/frontPublic/views/homePage/components/lunbo.vue
+++ src/views/frontPublic/views/homePage/components/lunbo.vue
@@ -21,7 +21,7 @@
         </a>
       </div>
     </a-carousel>
-    {{pageType}}
+<!--    {{pageType}}-->
     <LoginPanel
       v-if="pageType == 'login'"
       v-show="isInIndexPage"
@@ -99,18 +99,18 @@ export default {
      * @description 检查轮播图链接是否有效
      * @returns {Bool} 返回校验结果
      */
-    IsURL (str_url) { 
-      var strRegex = '^((https|http|ftp|rtsp|mms)?://)' 
-      + '?(([0-9a-z_!~*\'().&=+$%-]+: )?[0-9a-z_!~*\'().&=+$%-]+@)?' //ftp的user@ 
-      + '(([0-9]{1,3}.){3}[0-9]{1,3}' // IP形式的URL- 199.194.52.184 
-      + '|' // 允许IP和DOMAIN（域名） 
-      + '([0-9a-z_!~*\'()-]+.)*' // 域名- www. 
-      + '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' // 二级域名 
-      + '[a-z]{2,6})' // first level domain- .com or .museum 
-      + '(:[0-9]{1,4})?' // 端口- :80 
-      + '((/?)|' // a slash isn't required if there is no file name 
-      + '(/[0-9a-zA-Z_!~*\'().;?:@&=+$,%#-]+)+/?)$' 
-      var re=new RegExp(strRegex) 
+    IsURL (str_url) {
+      var strRegex = '^((https|http|ftp|rtsp|mms)?://)'
+      + '?(([0-9a-z_!~*\'().&=+$%-]+: )?[0-9a-z_!~*\'().&=+$%-]+@)?' //ftp的user@
+      + '(([0-9]{1,3}.){3}[0-9]{1,3}' // IP形式的URL- 199.194.52.184
+      + '|' // 允许IP和DOMAIN（域名）
+      + '([0-9a-z_!~*\'()-]+.)*' // 域名- www.
+      + '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' // 二级域名
+      + '[a-z]{2,6})' // first level domain- .com or .museum
+      + '(:[0-9]{1,4})?' // 端口- :80
+      + '((/?)|' // a slash isn't required if there is no file name
+      + '(/[0-9a-zA-Z_!~*\'().;?:@&=+$,%#-]+)+/?)$'
+      var re=new RegExp(strRegex)
       return re.test(str_url)
     },
 
@@ -265,8 +265,8 @@ export default {
 
 .img-style {
   height: 400px;
-  margin: 0 auto; 
-  width: 100%; 
+  margin: 0 auto;
+  width: 100%;
   object-fit: cover;
 }
 </style>
