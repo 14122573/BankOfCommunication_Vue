a15d111c8a2be394f6bd2da02a6806be5aed1c2a
Gakki <163alliance@gmail.com>
2020-10-13 17:56:47 +0800

    透明度

--- src/views/login/components/footer.vue
+++ src/views/login/components/footer.vue
@@ -10,7 +10,7 @@ export default {
 </script>
 
 <style scoped>
-.footer { height: 50px; width: 100%; text-align: center; position: absolute; bottom: 0px; left: 0px; color: #cfd7f3; font-size: 14px;}
+.footer { height: 50px; width: 100%; text-align: center; position: absolute; bottom: 0px; left: 0px; color: #cfd7f3; font-size: 14px; background-color: rgba(170, 171, 172, 0.4)}
 .footer p {	margin: 0; }
 .footer p:last-of-type { 	color: #829bdc; }
 </style>
