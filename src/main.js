ad47a68e0de7801bf4ac4816f9b332f1a2f60eaf
13397728679 <xx-y-c@outlook.com>
2021-01-25 11:53:44 +0800

    test2

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import ajax from '@/server/ajax'
 import api from '@/server/api'
 import titleId from '@/config/titleManageId'
 import { registerMicroApps, start } from 'qiankun'
-import dataJson from './config/System.json'
+// import dataJson from './config/System.json'
 import PermissionControl from './util/permission-control.js' // 权限自定义指令 v-permission="code"
 import { PermissionFilter } from './util/permission-filter.js' // 权限全局方法 v-if="$permission('code')"
 import {
@@ -183,7 +183,7 @@ const render = async ({ appContent, loading } = {}) => {
 (async function loadMicsystem() {
   let MicRouters = (await Axios.get(api.CONFIGS_MICSYSTEMS_LIST)).data
 
-  let system = Object.assign({}, dataJson)
+  let system = Object.assign({}, MicRouters)
 
   let projects = []
   for (let i = 0; i < system.sit.length; i++) {
--- src/router/getMicRouters.js
+++ src/router/getMicRouters.js
@@ -1,14 +1,14 @@
 import Axios from 'axios'
 import TipsOutsite from '@/views/tips/outsite'
 import api from '@/server/api'
-
+import dataJson from './micRouters1.json'
 async function GetRoutes(router) {
   // const MicRouters = require('@/router/micRouter.json')
   const rv = Math.floor(Math.random() * Math.random() * 10000)
   const MicRouters = (
     await Axios.get(api.CONFIGS_MICSYSTEMS_ROUTERS + '?v=' + rv)
   ).data
-  const micSystemRoutersConfigs = Object.assign({}, MicRouters)
+  const micSystemRoutersConfigs = Object.assign({}, dataJson)
   const { routes } = router.options
   const parent = routes.find(item => item.name === 'Layout')
   for (let key in micSystemRoutersConfigs) {
